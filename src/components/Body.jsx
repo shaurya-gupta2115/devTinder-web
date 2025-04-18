15577bcac3534161dcf9d385beb68233b9a1eb2f
Shaurya Gupta <vishnu8858369983@gmail.com>
2025-04-19 04:49:18 +0530

    if token not present --> login and avoided extra api call to go from one path to another once user is fetched

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,35 +4,39 @@ import Footer from "./Footer";
 import { Outlet, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
-
-
   //what happens is that when we refresh the page then the data got offload and store get empetied to avoid this we are building this feature
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
 
   const fetchUser = async () => {
     try {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
-      dispatch(addUser(res.data))
+      dispatch(addUser(res.data));
     } catch (err) {
-      navigate("/login")
-      console.error(err)
+      if (err.status === 401) {
+        //here we are going to check whether the token is present or not ...if the token is not present and you want to access the
+        // api then it will response by sending unauthorissed access error due to which 401 will get matched and will navigate to the login page
+        navigate("/login");
+      }
+      console.error(err);
     }
   };
 
   // i have to now build functionality in which whenever i reload the page .... fetchuser get run automatically ;
-
-  useEffect(()=> {
-    fetchUser();
-  },[]);
+  useEffect(() => {
+    if (!userData) {
+      fetchUser(); //this is to check when we have the user data once fetched and stored in redux store ..if user data is not there ..then on refresh we have to call this
+      // other wise once data is got at the store then there is no any demand to fetch the data again and agaiin ... it can be directly access from redux-store
+    }
+  }, []);
 
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
@@ -64,10 +65,10 @@ const Navbar = () => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
